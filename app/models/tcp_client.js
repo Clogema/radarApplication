a81d92694ad08c768e213d71153b9c9c791420dd
maevepetit83 <maevepetit83@gmail.com>
2019-01-16 15:16:14 +0100

    Ajout du PIR dans tcp_client.js

--- app/models/tcp_client.js
+++ app/models/tcp_client.js
@@ -1,9 +1,10 @@
 var xyp =0; // x si 0 | y si 1
 
-var address = '192.168.43.108';
+var address = '192.168.0.107';
 //var address = '127.0.0.1';
-var port = 3000;
-
+//var address = '172.20.10.5';
+var port_radar = 3000;
+var port_PIR = 3500;
 /*var net = require('net');
 
 var server = net.createServer(function(socket) {
@@ -16,16 +17,29 @@ server.listen(port, address);*/
 
 var net = require('net');
 
-var client = new net.Socket();
+var client_radar = new net.Socket();
+var client_PIR = new net.Socket();
 
 //client.bytesRead = 2;
 
-client.connect(port, address, function() {
+// Connexion RADAR :
+client_radar.connect(port_radar, address, function() {
 	console.log('Connected');
-	client.write('Hello, server! Love, Client.');
+	client_radar.write('Hello, server! Love, Client.');
 });
 
-client.on('data', function(data) {
+
+// Connexion PIR :
+client_PIR.connect(port_PIR, address, function() {
+	console.log('Connected');
+	client_PIR.write('Hello, server! Love, Client.');
+});
+
+
+
+
+// Réception données RADAR :
+client_radar.on('data', function(data) {
 
 	var dataS = data.toString();
 
@@ -41,18 +55,45 @@ client.on('data', function(data) {
 			console.log('y = ' +  dataSplit[i]);
 			xyp = 2;
 		} else if (xyp == 2) {
-			console.log('peak = ' +  dataSplit[i] + '\n');
+			console.log('peak = ' +  dataSplit[i]);
+			xyp = 3;
+		} else if (xyp == 3) {
+			console.log('rangeID = ' +  dataSplit[i]);
+			xyp = 4;
+		} else if (xyp == 4) {
+			console.log('Doppler = ' +  dataSplit[i] + '\n');
 			xyp = 0;
+
 		} else {
 			console.log("ERROR");
 		}		
 	}
 
 	if (data == "END") {
-		client.destroy();
+		client_radar.destroy();
+		client_PIR.destroy(); //On arrête le PIR lorsque le radar a fini.
 	}
 });
 
-client.on('close', function() {
+client_radar.on('close', function() {
+	console.log('Connection closed');
+});
+
+// Réception données PIR :
+client_PIR.on('data', function(data) {
+
+	var dataS = data.toString();
+	//console.log('PIR = ', dataS);
+
+	dataSplit = dataS.split('\n');
+
+
+	for(i = 0; i < dataSplit.length; i++) {
+		
+		console.log('PIR = ', dataSplit[i]);
+	}
+});
+
+client_PIR.on('close', function() {
 	console.log('Connection closed');
 });
